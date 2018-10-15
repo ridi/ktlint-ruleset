ddfaf5226db3a51f1aff8bee203d5223d645f5f8
kering <kering@ridi.com>
2018-10-15 15:42:48 +0900

    Update code style.

--- src/test/kotlin/com/ridi/ktlint/MultilineArgumentsRuleTest.kt
+++ src/test/kotlin/com/ridi/ktlint/MultilineArgumentsRuleTest.kt
@@ -7,84 +7,82 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 
-class MultilineArgumentsRuleTest : Spek(
-    {
-        describe("multiline-arguments rule") {
-            // whenever KTLINT_DEBUG env variable is set to "ast" or -DktlintDebug=ast is used
-            // com.github.shyiko.ktlint.test.(lint|format) will print AST (along with other debug info) to the stderr.
-            // this can be extremely helpful while writing and testing rules.
-            // uncomment the line below to take a quick look at it
-            // System.setProperty("ktlintDebug", "ast")
+class MultilineArgumentsRuleTest : Spek({
+    describe("multiline-arguments rule") {
+        // whenever KTLINT_DEBUG env variable is set to "ast" or -DktlintDebug=ast is used
+        // com.github.shyiko.ktlint.test.(lint|format) will print AST (along with other debug info) to the stderr.
+        // this can be extremely helpful while writing and testing rules.
+        // uncomment the line below to take a quick look at it
+        // System.setProperty("ktlintDebug", "ast")
 
-            val rule = MultilineArgumentsRule()
+        val rule = MultilineArgumentsRule()
 
-            it(
-                """
-                should check line breaks both after the left parenthesis and
-                before the right parenthesis in multiline arguments.
-                """.trimIndent()
-            ) {
-                Assertions.assertThat(
-                    rule.lint(
-                        """
-                        fun foo() {
-                            bar(1, 2, "", false)
-                            bar(
-                                1, 2, "", false)
-                            bar(
-                               1, 2, "", false
+        it(
+            """
+            should check line breaks both after the left parenthesis and
+            before the right parenthesis in multiline arguments.
+            """.trimIndent()
+        ) {
+            Assertions.assertThat(
+                rule.lint(
+                    """
+                    fun foo() {
+                        bar(1, 2, "", false)
+                        bar(
+                            1, 2, "", false)
+                        bar(
+                           1, 2, "", false
+                        )
+                        bar(1, 2,
+                            "", false)
+                        bar(
+                            1, 2,
+                            "", false
                             )
-                            bar(1, 2,
-                                "", false)
+                        baz(bar(
+                            1, 2, "", false
+                        ))
+                        baz(
                             bar(
-                                1, 2,
-                                "", false
-                                )
-                            baz(bar(
                                 1, 2, "", false
-                            ))
-                            baz(
-                                bar(
-                                    1, 2, "", false
-                                )
                             )
-                            qux(object : A {
-                            }, 1, 2)
-                            qux(
-                                object : A {
-                                },
-                                1, 2
-                            )
-                            qux(object : A {
+                        )
+                        qux(object : A {
+                        }, 1, 2)
+                        qux(
+                            object : A {
                             },
-                            1, 2)
-                            quxx(1, 2, { _ ->
+                            1, 2
+                        )
+                        qux(object : A {
+                        },
+                        1, 2)
+                        quxx(1, 2, { _ ->
+                        })
+                        quxx(
+                            1, 2,
+                            { _ ->
+                            }
+                        )
+                        quxx(1, 2,
+                            { _ ->
                             })
-                            quxx(
-                                1, 2,
-                                { _ ->
-                                }
-                            )
-                            quxx(1, 2,
-                                { _ ->
-                                })
-                        }
-                        """.trimIndent()
-                    )
-                ).isEqualTo(
-                    listOf(
-                        LintError(4, 23, rule.id, "No line break before the right parenthesis in multiline arguments."),
-                        LintError(8, 9, rule.id, "No line break after the left parenthesis in multiline arguments."),
-                        LintError(9, 17, rule.id, "No line break before the right parenthesis in multiline arguments."),
-                        LintError(14, 9, rule.id, "No line break after the left parenthesis in multiline arguments."),
-                        LintError(16, 5, rule.id, "No line break before the right parenthesis in multiline arguments."),
-                        LintError(29, 9, rule.id, "No line break after the left parenthesis in multiline arguments."),
-                        LintError(31, 8, rule.id, "No line break before the right parenthesis in multiline arguments."),
-                        LintError(39, 10, rule.id, "No line break after the left parenthesis in multiline arguments."),
-                        LintError(41, 9, rule.id, "No line break before the right parenthesis in multiline arguments.")
-                    )
+                    }
+                    """.trimIndent()
                 )
-            }
+            ).isEqualTo(
+                listOf(
+                    LintError(4, 23, rule.id, "No line break before the right parenthesis in multiline arguments."),
+                    LintError(8, 9, rule.id, "No line break after the left parenthesis in multiline arguments."),
+                    LintError(9, 17, rule.id, "No line break before the right parenthesis in multiline arguments."),
+                    LintError(14, 9, rule.id, "No line break after the left parenthesis in multiline arguments."),
+                    LintError(16, 5, rule.id, "No line break before the right parenthesis in multiline arguments."),
+                    LintError(29, 9, rule.id, "No line break after the left parenthesis in multiline arguments."),
+                    LintError(31, 8, rule.id, "No line break before the right parenthesis in multiline arguments."),
+                    LintError(39, 10, rule.id, "No line break after the left parenthesis in multiline arguments."),
+                    LintError(41, 9, rule.id, "No line break before the right parenthesis in multiline arguments.")
+                )
+            )
         }
     }
-)
+})
--- src/test/kotlin/com/ridi/ktlint/NoExclamationNotOperatorRuleTest.kt
+++ src/test/kotlin/com/ridi/ktlint/NoExclamationNotOperatorRuleTest.kt
@@ -7,33 +7,31 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 
-class NoExclamationNotOperatorRuleTest : Spek(
-    {
-        describe("no-exclamation-not-operator rule") {
-            // whenever KTLINT_DEBUG env variable is set to "ast" or -DktlintDebug=ast is used
-            // com.github.shyiko.ktlint.test.(lint|format) will print AST (along with other debug info) to the stderr.
-            // this can be extremely helpful while writing and testing rules.
-            // uncomment the line below to take a quick look at it
-            // System.setProperty("ktlintDebug", "ast")
+class NoExclamationNotOperatorRuleTest : Spek({
+    describe("no-exclamation-not-operator rule") {
+        // whenever KTLINT_DEBUG env variable is set to "ast" or -DktlintDebug=ast is used
+        // com.github.shyiko.ktlint.test.(lint|format) will print AST (along with other debug info) to the stderr.
+        // this can be extremely helpful while writing and testing rules.
+        // uncomment the line below to take a quick look at it
+        // System.setProperty("ktlintDebug", "ast")
 
-            val rule = NoExclamationNotOperatorRule()
+        val rule = NoExclamationNotOperatorRule()
 
-            it("should prohibit usage of ! operator") {
-                Assertions.assertThat(
-                    rule.lint(
-                        """
-                        fun fn() {
-                           val a = true
-                           val b = !a
-                           val c = a != b
-                           val d: String? = ""
-                           val e = d!!
-                           val f = "!"
-                        }
-                        """.trimIndent()
-                    )
-                ).isEqualTo(listOf(LintError(3, 12, rule.id, "Unexpected !, use .not() instead")))
-            }
+        it("should prohibit usage of ! operator") {
+            Assertions.assertThat(
+                rule.lint(
+                    """
+                    fun fn() {
+                       val a = true
+                       val b = !a
+                       val c = a != b
+                       val d: String? = ""
+                       val e = d!!
+                       val f = "!"
+                    }
+                    """.trimIndent()
+                )
+            ).isEqualTo(listOf(LintError(3, 12, rule.id, "Unexpected !, use .not() instead")))
         }
     }
-)
+})
